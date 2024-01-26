1d106f15791a942eba0ba36ce4fbc3101e5b3af4
Björn Ritzl <bjorn.ritzl@gmail.com>
2024-01-26 09:41:37 +0100

    Added more tests

---
+++ tests/factory/properties.go
@@ -0,0 +1,15 @@
+components {
+  id: "properties"
+  component: "/tests/factory/properties.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
--- tests/test.lua
+++ tests/test.lua
@@ -27,38 +27,44 @@ function M.async(fn)
 	assert(coroutine.resume(co, yield, resume))
 end
 
-function M.wait(delay_or_condition)
+function M.wait_until_true(condition)
 	local co = coroutine.running()
 	assert(co, "You must call function from within a coroutine")
+	assert(type(condition) == "function")
 
-	if type(delay_or_condition) == "function" then
-		local condition = delay_or_condition
-		timer.delay(0, true, function(self, handle, time_elapsed)
-			if condition() then
-				timer.cancel(handle)
-				assert(coroutine.resume(co))
-			end
-		end)
-		coroutine.yield()
-	else
-		local delay = delay_or_condition or 0
-		timer.delay(delay, false, function()
-			assert(coroutine.resume(co))
-		end)
-		coroutine.yield()
-	end
-end
-
-function M.condition(fn)
-	local co = coroutine.running()
-	assert(co, "You must call yield() from within a coroutine")
 	timer.delay(0, true, function(self, handle, time_elapsed)
-		if fn() then
+		if condition() then
 			timer.cancel(handle)
 			assert(coroutine.resume(co))
 		end
 	end)
 	coroutine.yield()
+end
+
+function M.wait_until_done(fn)
+	local co = coroutine.running()
+	assert(co, "You must call function from within a coroutine")
+	assert(type(fn) == "function")
+
+	fn(function()
+		assert(coroutine.resume(co))
+	end)
+	coroutine.yield()
+end
+
+function M.wait(delay)
+	local co = coroutine.running()
+	assert(co, "You must call function from within a coroutine")
+	delay = delay or 0
+	timer.delay(delay, false, function()
+		assert(coroutine.resume(co))
+	end)
+	coroutine.yield()
+end
+
+function M.assert_error(fn, ...)
+	local ok, err = pcall(fn, ...)
+	assert(not ok, "Expected function to generate an error")
 end
 
 function M.ok()
