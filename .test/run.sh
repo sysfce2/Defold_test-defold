40a39db04e0136d37066509abe1786caa19c2e95
Björn Ritzl <bjorn.ritzl@gmail.com>
2024-01-28 14:24:03 +0100

    Update run.sh

--- .test/run.sh
+++ .test/run.sh
@@ -4,28 +4,30 @@ if [ $# -eq 0 ]; then
 else
 	PLATFORM="$1"
 fi
-
-
 echo "${PLATFORM}"
+
+if [ "${PLATFORM}" = "x86_64-win32" ] || [ "${PLATFORM}" = "x86-win32" ]; then
+	DMENGINE=dmengine_headless.exe
+else
+	DMENGINE=dmengine_headless
+fi
 
 # {"version": "1.2.89", "sha1": "5ca3dd134cc960c35ecefe12f6dc81a48f212d40"}
 # Get SHA1 of the current Defold stable release
-SHA1=$(curl -s http://d.defold.com/stable/info.json | sed 's/.*sha1": "\(.*\)".*/\1/')
-echo "Using Defold dmengine_headless version ${SHA1}"
+SHA1=$(curl -s http://d.defold.com/alpha/info.json | sed 's/.*sha1": "\(.*\)".*/\1/')
+echo "Using Defold ${DMENGINE} version ${SHA1}"
 
-# Create dmengine_headless and bob.jar URLs
-DMENGINE_URL="http://d.defold.com/archive/${SHA1}/engine/${PLATFORM}/dmengine_headless"
-BOB_URL="http://d.defold.com/archive/${SHA1}/bob/bob.jar"
-
-# Download dmengine_headless
-if [[ ! -f dmengine_headless ]]; then
+# Download dmengine
+if [[ ! -f ${DMENGINE} ]]; then
+	DMENGINE_URL="http://d.defold.com/archive/${SHA1}/engine/${PLATFORM}/${DMENGINE}"
 	echo "Downloading ${DMENGINE_URL}"
-	curl -L -o dmengine_headless ${DMENGINE_URL}
-	chmod +x dmengine_headless
+	curl -L -o ${DMENGINE} ${DMENGINE_URL}
+	chmod +x ${DMENGINE}
 fi
 
 # Download bob.jar
 if [[ ! -f bob.jar ]]; then
+	BOB_URL="http://d.defold.com/archive/${SHA1}/bob/bob.jar"
 	echo "Downloading ${BOB_URL}"
 	curl -L -o bob.jar ${BOB_URL}
 fi
@@ -37,9 +39,9 @@ java -jar bob.jar resolve
 echo "Running bob.jar - building"
 java -jar bob.jar --verbose --variant=debug build --platform=${PLATFORM} --keep-unused
 
-echo "Starting dmengine_headless"
+echo "Starting dmengine"
 if [ -n "${DEFOLD_BOOSTRAP_COLLECTION}" ]; then
-	./dmengine_headless --config=bootstrap.main_collection=${DEFOLD_BOOSTRAP_COLLECTION}
+	./${DMENGINE} --config=bootstrap.main_collection=${DEFOLD_BOOSTRAP_COLLECTION}
 else
-	./dmengine_headless
+	./${DMENGINE}
 fi
