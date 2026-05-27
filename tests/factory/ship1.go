fafb729cd2ee154d2958f8c709034e909cb6d7d1
AGulev <me@agulev.com>
2026-05-27 22:25:19 +0200

    Defold 1.13.0 update: rmeove old APIs

--- tests/factory/ship1.go
+++ tests/factory/ship1.go
@@ -1,20 +1,11 @@
 embedded_components {
   id: "sprite"
   type: "sprite"
-  data: "tile_set: \"/assets/single.atlas\"\n"
-  "default_animation: \"shipBlue_manned\"\n"
+  data: "default_animation: \"shipBlue_manned\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
-  "blend_mode: BLEND_MODE_ALPHA\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/assets/single.atlas\"\n"
+  "}\n"
   ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
 }
